0425664cfec8acbdfabe5c2108839411c8341059
SIMHENDRA <shyamnatarajan1@gmail.com>
2020-10-17 14:33:15 -0400

    Update targets.py

--- targets.py
+++ targets.py
@@ -80,12 +80,12 @@ def changeBounty(name, BV):
 
 
 def targetsToPrint():
-    comd = "SELECT target, bounty_amt, kill_count, notes FROM targets ORDER BY bounty_amt DESC"
+    comd = "SELECT target, bounty_amt, kill_count FROM targets ORDER BY bounty_amt DESC"
     conn = sqlite3.connect(dbpath)
     c = conn.cursor()
     ret = c.execute(comd).fetchall()
     conn.close()
-    header = ["TARGET", "VALUE", "DEATHS", "NOTES"]
+    header = ["TARGET", "VALUE", "DEATHS"]
     first = tuple(header)
     ret.insert(0, first)
     return ret
